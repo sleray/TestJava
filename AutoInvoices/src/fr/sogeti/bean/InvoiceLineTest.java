7b7b55d7d81062db34d2488dedfbb7de701528cd
Sylvain Leray <syleray@gmail.com>
2020-07-01 14:54:59 +0200

    Adding Unit tests

--- AutoInvoices/src/fr/sogeti/bean/InvoiceLineTest.java
+++ AutoInvoices/src/fr/sogeti/bean/InvoiceLineTest.java
@@ -7,6 +7,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
+import fr.sogeti.enums.ProductType;
+
 /**
  * @author sleray
  *
@@ -19,11 +21,36 @@ class InvoiceLineTest {
 	@Test
 	void testRoundTaxAmount() {		
 		InvoiceLine il = new InvoiceLine();
-		assertEquals( 12.5f,il.roundTaxAmount(12.49f));
-		assertEquals( 1.0f,il.roundTaxAmount(0.99f));
-		assertEquals( 1.0f,il.roundTaxAmount(1.0f));
-		assertEquals( 1.05f,il.roundTaxAmount(1.01f));
-		assertEquals( 1.05f,il.roundTaxAmount(1.02f));
+		assertEquals( 12.5d,il.roundTaxAmount(12.49d));
+		assertEquals( 1.0d,il.roundTaxAmount(0.99d));
+		assertEquals( 1.0d,il.roundTaxAmount(1.0d));
+		assertEquals( 1.05d,il.roundTaxAmount(1.01d));
+		assertEquals( 1.05d,il.roundTaxAmount(1.02d));
+		
+	}
+	@Test
+	void testSingleProductsSimpleTaxes() {
+		//Test different Taxes patterns
+		Product simpleFood = new Product("Simple food",ProductType.FOOD,100d,false);
+		InvoiceLine il = new InvoiceLine(simpleFood,1);
+		assertEquals(100d,il.getTotalWithTaxes());
+		assertEquals(0d,il.getTaxes());
+		
+		Product simpleDrug = new Product("Simple drug",ProductType.DRUG,100d,false);
+		InvoiceLine il2 = new InvoiceLine(simpleDrug,1);
+		assertEquals(100d,il2.getTotalWithTaxes());
+		assertEquals(0d,il2.getTaxes());
+		
+		Product simpleBook = new Product("Simple book",ProductType.BOOK,100d,false);
+		InvoiceLine il3 = new InvoiceLine(simpleBook,1);
+		assertEquals(110d,il3.getTotalWithTaxes());
+		assertEquals(10d,il3.getTaxes());
+		
+		Product simpleDefault = new Product("Simple Other",ProductType.DEFAULT,100d,false);
+		InvoiceLine il4 = new InvoiceLine(simpleDefault,1);
+		assertEquals(120d,il4.getTotalWithTaxes());
+		assertEquals(10d,il4.getTaxes());
+		
 		
 	}
 
